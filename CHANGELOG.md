9f44727f33a06c7f66a2c59a735460aaa472dba8
Benjamin Heisig <benjamin@heisig.name>
2020-12-02 11:45:39 +0100

    Add i-doit open, version `1.15.2`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,13 +9,17 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ### Added
 
+-   Add i-doit open, version `1.15.2`
 -   Add i-doit pro, version `1.15.2`
 
 ## [0.3.0][] – 2020-10-23
 
 ### Added
 
--   Add i-doit open/pro, versions `1.15` and `1.15.1`
+-   Add i-doit open, version `1.15.1`
+-   Add i-doit pro, version `1.15.1`
+-   Add i-doit open, version `1.15`
+-   Add i-doit pro, version `1.15`
 -   Add i-doit open, version `1.14.2`
 
 ### Fixed
--- docker.sh
+++ docker.sh
@@ -168,6 +168,15 @@ function buildImages {
     buildImage 1.15.2 pro php7.3 fpm
     buildImage 1.15.2 pro php7.4 apache
     buildImage 1.15.2 pro php7.4 fpm
+
+    buildImage 1.15.2 open php7.1 apache
+    buildImage 1.15.2 open php7.1 fpm
+    buildImage 1.15.2 open php7.2 apache
+    buildImage 1.15.2 open php7.2 fpm
+    buildImage 1.15.2 open php7.3 apache
+    buildImage 1.15.2 open php7.3 fpm
+    buildImage 1.15.2 open php7.4 apache
+    buildImage 1.15.2 open php7.4 fpm
 }
 
 function pullImages {
@@ -309,6 +318,15 @@ function scanImages {
     scanImage 1.15.2 pro php7.3 fpm
     scanImage 1.15.2 pro php7.4 apache
     scanImage 1.15.2 pro php7.4 fpm
+
+    scanImage 1.15.2 open php7.1 apache
+    scanImage 1.15.2 open php7.1 fpm
+    scanImage 1.15.2 open php7.2 apache
+    scanImage 1.15.2 open php7.2 fpm
+    scanImage 1.15.2 open php7.3 apache
+    scanImage 1.15.2 open php7.3 fpm
+    scanImage 1.15.2 open php7.4 apache
+    scanImage 1.15.2 open php7.4 fpm
 }
 
 function scanImage {
@@ -472,86 +490,14 @@ function fixFilePermissions {
 function pushImages {
     log "Push Docker images to registry…"
 
-    pushImage 1.15.2 pro php7.1 apache
-    pushImage 1.15.2 pro php7.1 fpm
-    pushImage 1.15.2 pro php7.2 apache
-    pushImage 1.15.2 pro php7.2 fpm
-    pushImage 1.15.2 pro php7.3 apache
-    pushImage 1.15.2 pro php7.3 fpm
-    pushImage 1.15.2 pro php7.4 apache
-    pushImage 1.15.2 pro php7.4 fpm
-
-    pushImage 1.15.1 open php7.1 apache
-    pushImage 1.15.1 open php7.1 fpm
-    pushImage 1.15.1 open php7.2 apache
-    pushImage 1.15.1 open php7.2 fpm
-    pushImage 1.15.1 open php7.3 apache
-    pushImage 1.15.1 open php7.3 fpm
-    pushImage 1.15.1 open php7.4 apache
-    pushImage 1.15.1 open php7.4 fpm
-
-    pushImage 1.15.1 pro php7.1 apache
-    pushImage 1.15.1 pro php7.1 fpm
-    pushImage 1.15.1 pro php7.2 apache
-    pushImage 1.15.1 pro php7.2 fpm
-    pushImage 1.15.1 pro php7.3 apache
-    pushImage 1.15.1 pro php7.3 fpm
-    pushImage 1.15.1 pro php7.4 apache
-    pushImage 1.15.1 pro php7.4 fpm
-
-    pushImage 1.15 open php7.1 apache
-    pushImage 1.15 open php7.1 fpm
-    pushImage 1.15 open php7.2 apache
-    pushImage 1.15 open php7.2 fpm
-    pushImage 1.15 open php7.3 apache
-    pushImage 1.15 open php7.3 fpm
-    pushImage 1.15 open php7.4 apache
-    pushImage 1.15 open php7.4 fpm
-
-    pushImage 1.15 pro php7.1 apache
-    pushImage 1.15 pro php7.1 fpm
-    pushImage 1.15 pro php7.2 apache
-    pushImage 1.15 pro php7.2 fpm
-    pushImage 1.15 pro php7.3 apache
-    pushImage 1.15 pro php7.3 fpm
-    pushImage 1.15 pro php7.4 apache
-    pushImage 1.15 pro php7.4 fpm
-
-    pushImage 1.14.2 open php7.1 apache
-    pushImage 1.14.2 open php7.1 fpm
-    pushImage 1.14.2 open php7.2 apache
-    pushImage 1.14.2 open php7.2 fpm
-    pushImage 1.14.2 open php7.3 apache
-    pushImage 1.14.2 open php7.3 fpm
-    pushImage 1.14.2 open php7.4 apache
-    pushImage 1.14.2 open php7.4 fpm
-
-    pushImage 1.14.2 pro php7.1 apache
-    pushImage 1.14.2 pro php7.1 fpm
-    pushImage 1.14.2 pro php7.2 apache
-    pushImage 1.14.2 pro php7.2 fpm
-    pushImage 1.14.2 pro php7.3 apache
-    pushImage 1.14.2 pro php7.3 fpm
-    pushImage 1.14.2 pro php7.4 apache
-    pushImage 1.14.2 pro php7.4 fpm
-
-    pushImage 1.14.1 open php7.1 apache
-    pushImage 1.14.1 open php7.1 fpm
-    pushImage 1.14.1 open php7.2 apache
-    pushImage 1.14.1 open php7.2 fpm
-    pushImage 1.14.1 open php7.3 apache
-    pushImage 1.14.1 open php7.3 fpm
-    pushImage 1.14.1 open php7.4 apache
-    pushImage 1.14.1 open php7.4 fpm
-
-    pushImage 1.14.1 pro php7.1 apache
-    pushImage 1.14.1 pro php7.1 fpm
-    pushImage 1.14.1 pro php7.2 apache
-    pushImage 1.14.1 pro php7.2 fpm
-    pushImage 1.14.1 pro php7.3 apache
-    pushImage 1.14.1 pro php7.3 fpm
-    pushImage 1.14.1 pro php7.4 apache
-    pushImage 1.14.1 pro php7.4 fpm
+    pushImage 1.14 pro php7.1 apache
+    pushImage 1.14 pro php7.1 fpm
+    pushImage 1.14 pro php7.2 apache
+    pushImage 1.14 pro php7.2 fpm
+    pushImage 1.14 pro php7.3 apache
+    pushImage 1.14 pro php7.3 fpm
+    pushImage 1.14 pro php7.4 apache
+    pushImage 1.14 pro php7.4 fpm
 
     pushImage 1.14 open php7.1 apache
     pushImage 1.14 open php7.1 fpm
@@ -562,14 +508,95 @@ function pushImages {
     pushImage 1.14 open php7.4 apache
     pushImage 1.14 open php7.4 fpm
 
-    pushImage 1.14 pro php7.1 apache
-    pushImage 1.14 pro php7.1 fpm
-    pushImage 1.14 pro php7.2 apache
-    pushImage 1.14 pro php7.2 fpm
-    pushImage 1.14 pro php7.3 apache
-    pushImage 1.14 pro php7.3 fpm
-    pushImage 1.14 pro php7.4 apache
-    pushImage 1.14 pro php7.4 fpm
+    pushImage 1.14.1 pro php7.1 apache
+    pushImage 1.14.1 pro php7.1 fpm
+    pushImage 1.14.1 pro php7.2 apache
+    pushImage 1.14.1 pro php7.2 fpm
+    pushImage 1.14.1 pro php7.3 apache
+    pushImage 1.14.1 pro php7.3 fpm
+    pushImage 1.14.1 pro php7.4 apache
+    pushImage 1.14.1 pro php7.4 fpm
+
+    pushImage 1.14.1 open php7.1 apache
+    pushImage 1.14.1 open php7.1 fpm
+    pushImage 1.14.1 open php7.2 apache
+    pushImage 1.14.1 open php7.2 fpm
+    pushImage 1.14.1 open php7.3 apache
+    pushImage 1.14.1 open php7.3 fpm
+    pushImage 1.14.1 open php7.4 apache
+    pushImage 1.14.1 open php7.4 fpm
+
+    pushImage 1.14.2 pro php7.1 apache
+    pushImage 1.14.2 pro php7.1 fpm
+    pushImage 1.14.2 pro php7.2 apache
+    pushImage 1.14.2 pro php7.2 fpm
+    pushImage 1.14.2 pro php7.3 apache
+    pushImage 1.14.2 pro php7.3 fpm
+    pushImage 1.14.2 pro php7.4 apache
+    pushImage 1.14.2 pro php7.4 fpm
+
+    pushImage 1.14.2 open php7.1 apache
+    pushImage 1.14.2 open php7.1 fpm
+    pushImage 1.14.2 open php7.2 apache
+    pushImage 1.14.2 open php7.2 fpm
+    pushImage 1.14.2 open php7.3 apache
+    pushImage 1.14.2 open php7.3 fpm
+    pushImage 1.14.2 open php7.4 apache
+    pushImage 1.14.2 open php7.4 fpm
+
+    pushImage 1.15 pro php7.1 apache
+    pushImage 1.15 pro php7.1 fpm
+    pushImage 1.15 pro php7.2 apache
+    pushImage 1.15 pro php7.2 fpm
+    pushImage 1.15 pro php7.3 apache
+    pushImage 1.15 pro php7.3 fpm
+    pushImage 1.15 pro php7.4 apache
+    pushImage 1.15 pro php7.4 fpm
+
+    pushImage 1.15 open php7.1 apache
+    pushImage 1.15 open php7.1 fpm
+    pushImage 1.15 open php7.2 apache
+    pushImage 1.15 open php7.2 fpm
+    pushImage 1.15 open php7.3 apache
+    pushImage 1.15 open php7.3 fpm
+    pushImage 1.15 open php7.4 apache
+    pushImage 1.15 open php7.4 fpm
+
+    pushImage 1.15.1 pro php7.1 apache
+    pushImage 1.15.1 pro php7.1 fpm
+    pushImage 1.15.1 pro php7.2 apache
+    pushImage 1.15.1 pro php7.2 fpm
+    pushImage 1.15.1 pro php7.3 apache
+    pushImage 1.15.1 pro php7.3 fpm
+    pushImage 1.15.1 pro php7.4 apache
+    pushImage 1.15.1 pro php7.4 fpm
+
+    pushImage 1.15.1 open php7.1 apache
+    pushImage 1.15.1 open php7.1 fpm
+    pushImage 1.15.1 open php7.2 apache
+    pushImage 1.15.1 open php7.2 fpm
+    pushImage 1.15.1 open php7.3 apache
+    pushImage 1.15.1 open php7.3 fpm
+    pushImage 1.15.1 open php7.4 apache
+    pushImage 1.15.1 open php7.4 fpm
+
+    pushImage 1.15.2 pro php7.1 apache
+    pushImage 1.15.2 pro php7.1 fpm
+    pushImage 1.15.2 pro php7.2 apache
+    pushImage 1.15.2 pro php7.2 fpm
+    pushImage 1.15.2 pro php7.3 apache
+    pushImage 1.15.2 pro php7.3 fpm
+    pushImage 1.15.2 pro php7.4 apache
+    pushImage 1.15.2 pro php7.4 fpm
+
+    pushImage 1.15.2 open php7.1 apache
+    pushImage 1.15.2 open php7.1 fpm
+    pushImage 1.15.2 open php7.2 apache
+    pushImage 1.15.2 open php7.2 fpm
+    pushImage 1.15.2 open php7.3 apache
+    pushImage 1.15.2 open php7.3 fpm
+    pushImage 1.15.2 open php7.4 apache
+    pushImage 1.15.2 open php7.4 fpm
 }
 
 function pushImage {
@@ -586,6 +613,15 @@ function pushImage {
 }
 
 function printReadme {
+    printSupportedTags 1.15.2 open php7.4 fpm
+    printSupportedTags 1.15.2 open php7.4 apache
+    printSupportedTags 1.15.2 open php7.3 fpm
+    printSupportedTags 1.15.2 open php7.3 apache
+    printSupportedTags 1.15.2 open php7.2 fpm
+    printSupportedTags 1.15.2 open php7.2 apache
+    printSupportedTags 1.15.2 open php7.1 fpm
+    printSupportedTags 1.15.2 open php7.1 apache
+
     printSupportedTags 1.15.2 pro php7.4 fpm
     printSupportedTags 1.15.2 pro php7.4 apache
     printSupportedTags 1.15.2 pro php7.3 fpm
